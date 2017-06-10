6c263379e829f59d21c6b1b51a04c8b6b4778eb2
Javier Diaz <javier.diaz.palacios@gmail.com>
2017-06-10 11:38:10 -0400

    tokenize and radix sort

--- src/main/SuffixArray.java
+++ src/main/SuffixArray.java
@@ -1,77 +1,102 @@
 package main;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class SuffixArray {
   private final char SPEC = '$';
-  private final int ALPHABETSIZE = Character.getNumericValue('z') + 1;
+  private final int ALPHABETSIZE = Character.getNumericValue('z') + 2;
   private String text;
   
   public SuffixArray(String text) {
-    this.text = text;
-  }
-  
-  private String tokenizeGroup(String text, int group) {
-    List<Integer>[] bucketArray = new ArrayList[3];
-    
-    
-    int[] triple = new int[text.length()/3];
-
-    String currTriple;
-    for (int i = 2; i >= 0; i--) {
-      for (int j = group; j < triple.length; j++) {
-        currTriple = text.substring(j, j+3);
-      }
+    while (text.length()%3 != 1) {
+      text = text + SPEC;
     }
+    this.text = text;
     
-    
-    Comparator<Integer> cmp = new Comparator<Integer>() {
-      @Override
-      public int compare(Integer i0, Integer i1) {
-        String s0 = text.substring(i0, i0+3);
-        String s1 = text.substring(i1, i1+3);
-        return s0.compareTo(s1);
-      }
-    };
-    // FIXME hay que usar radix sort
-    return "";
+    int[] arr = {1, 4, 7, 10, 2, 5, 8};
+    System.out.println(tokenize(arr));
   }
   
-  public int[] radixSort(int[] indexArray) {
-    List<List<Integer>> bucketList = new ArrayList<List<Integer>>();
+  private String tokenize(int[] textIndexArray) {
+    char[] tokenArray = new char[textIndexArray.length];
+    int[] indexIndexArray = radixSort(textIndexArray);
+    int token = 0;
+    int index;
+    String lastTriple = null;
+    String currTriple;
+    
+    for (int i = 0; i < indexIndexArray.length; i++) {
+      index = textIndexArray[indexIndexArray[i]];
+      currTriple = this.text.substring(index, index + 3);
+      if (currTriple.equals(lastTriple)) {
+        tokenArray[indexIndexArray[i]] = Character.forDigit(token, 36);
+      } else {
+        tokenArray[indexIndexArray[i]] = Character.forDigit(++token, 36);
+      }
+      lastTriple = currTriple;
+    }
+    return new String(tokenArray);
+  }
+  
+  private int[] radixSort(int[] textIndexArray) {
+    List<List<Pair>> bucketList = new ArrayList<List<Pair>>();
+    Pair[] indexPairArray = new Pair[textIndexArray.length];
+    Pair indexPair;
     String triple;
     int index;
+    char symbol;
     
-    for (int i = 0; i < ALPHABETSIZE; i++) {
-      bucketList.add(new ArrayList<Integer>());
+    /* create an array of pairs (textIndex, indexIndex) */
+    for (int i = 0; i < indexPairArray.length; i++) {
+      indexPairArray[i] = new Pair(textIndexArray[i], i);
     }
     
+    /* initialize the list of buckets */
+    for (int i = 0; i < ALPHABETSIZE; i++) {
+      bucketList.add(new ArrayList<Pair>());
+    }
+    
+    /* put pairs of indexes in the buckets */
     for (int i = 2; i >= 0; i--) {
-      for (int j = 0; j < indexArray.length; j++) {
-        index = indexArray[j];
+      for (int j = 0; j < indexPairArray.length; j++) {
+        indexPair = indexPairArray[j];
+        index = indexPair.first;
         triple = this.text.substring(index, index + 3);
-        bucketList.get(Character.getNumericValue(triple.charAt(i))).add(index);
+        symbol = triple.charAt(i);
+        if (symbol == SPEC) {
+          bucketList.get(0).add(indexPair);
+        } else {
+          bucketList.get(Character.getNumericValue(symbol)).add(indexPair);
+        }
       }
       
       int c = 0;
-      System.out.println("ITER"+i+": ");
-      for (int j = 0; j < indexArray.length; j++) {
-        System.out.println(indexArray[j]);
-      }
       for (int j = 0; j < bucketList.size(); j++) {
         for (int k = 0; k < bucketList.get(j).size(); k++) {
-          System.out.println("  " + bucketList.get(j).get(k));
-          indexArray[c] = bucketList.get(j).get(k);
-          bucketList.get(j).clear();
+          indexPairArray[c] = bucketList.get(j).get(k);
+          c++;
         }
+        bucketList.get(j).clear();
       }
     }
-    return indexArray;
+    
+    /* extract the last index from the array of pairs */
+    int[] indexIndexArray = new int[indexPairArray.length];
+    for (int i = 0; i < indexPairArray.length; i++) {
+      indexIndexArray[i] = indexPairArray[i].last;
+    }
+    
+    return indexIndexArray;
   }
   
-  public String getText() {
-    return this.text;
+  private class Pair {
+    private final Integer first;
+    private final Integer last;
+    
+    private Pair(Integer first, Integer last) {
+      this.first = first;
+      this.last = last;
+    }
   }
 }
--- src/main/Test.java
+++ src/main/Test.java
@@ -4,18 +4,20 @@ import java.text.Normalizer;
 
 public class Test {
   public static void main(String[] args) {
-    String text = "aaaaabaac";
-    int[] indexArray = {0, 3, 6};
-    
-    text = normalizeText(text);
-    SuffixArray suf = new SuffixArray(text);
-    indexArray = suf.radixSort(indexArray);
+    testSuffixArray();
+  }
+  
+  @SuppressWarnings("unused")
+  private static void testSuffixArray() {
+    String text = "mississippi";
+    SuffixArray suf = new SuffixArray(normalizeText(text));
   }
   
   private static String normalizeText(String text) {
     text = text.toLowerCase();
     text = Normalizer.normalize(text, Normalizer.Form.NFD);
     text = text.replaceAll("[^0-9a-z ]", "");
+    text = text + "$";
     return text;
   }
 }
